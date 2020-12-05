aa8896edefef6bb64e6097a0ff9efe00e1df9f35
seanperryman94 <seanperryman94@gmail.com>
2020-12-05 12:34:28 -0500

    Add files via upload

---
+++ DataStructure1.cpp
@@ -0,0 +1,57 @@
+//#import stuff
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//Reading in data that looks like		SPG,2019-03-04 05:35,0.30
+
+//Different types of Nodes 
+//
+//			Head		(Pointer to variable number of Station)
+//			/    \
+//		 Station		(Static number of months)
+//         |	\
+//	     Month			(Psuedo Static number of days (Dependent on the month))
+//         |   \
+//		  day			(4 quartiles, static value)
+//		  /   \
+//	  Quartile ____		(4 data sets, static value)
+//		/ |   \		\
+//	Data  Data  Data  Data	(Variable number of data points)
+//
+
+vector<pair<string, int>> monthDayAssociation; //This establishes how many days are in a month
+
+struct quartNode {
+	int quartNumber = 0;//will be 1 2 3 or 4
+	vector<double> precipitation;
+	vector<double> humidity;
+	vector<double> windspeed;
+	vector<double> temperature;
+};
+
+struct dayNode {
+	int dayNumber = 0; //This value will be determined at creation
+	quartNode* children = NULL; //4 of these per day
+};
+
+struct monthNode {
+	string name;
+	dayNode* children = NULL//Needs to have monthDayAssociation[pair]->second number of children
+};
+
+
+struct stationNode {
+	string name;
+	monthNode* children = NULL //Needs to be 12 of these
+
+};
+
+struct headNode{
+
+	stationNode* child = NULL; //This pointer will need to be dynamically created, because there will be a non-constant number of children.
+	//All head node needs it to be able to dynamically point to more children nodes
+};
+
+
